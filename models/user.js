b900eb3a273e9e104d51c07aa242bfc4625aa79b
Ashutosh Kumar <worldismine9999@gmail.com>
2019-11-29 18:55:36 +0530

    improved constraints
    added some basic requirements

--- models/user.js
+++ models/user.js
@@ -2,8 +2,18 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-    username: String,
-    password: String
+    username: {
+        type: String,
+        required: true,
+        minlength: 3,
+        trim: true
+      },
+    password: {
+        type: String,
+        required: true,
+        minlength: 4,
+        trim: true
+      }
 });
 
 UserSchema.plugin(passportLocalMongoose)
