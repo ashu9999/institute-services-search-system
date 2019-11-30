e47ad9699db97258241bb33eeb02ed5212595a92
Ashutosh Kumar <worldismine9999@gmail.com>
2019-11-30 10:06:32 +0530

    removed data
    camp removed

--- seeds.js
+++ seeds.js
@@ -3,11 +3,7 @@ var Campground = require("./models/campground");
 var Comment   = require("./models/comment");
 
 var data = [
-    {
-        name: "Cloud's Rest", 
-        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-    },
+   
     {
         name: "Desert Mesa", 
         image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
