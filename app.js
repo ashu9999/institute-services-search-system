887eff8dbbdefe16f5b0c7128dc4473060d332a2
ashu9999 <worldismine9999@gmail.com>
2019-12-08 21:29:57 +0530

    app server fix

--- app.js
+++ app.js
@@ -50,5 +50,5 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 
 
 app.listen(3000, process.env.IP, function(){
-   console.log("The YelpCamp Server Has Started!");
+   console.log("The ISSS Server Has Started!");
 });
